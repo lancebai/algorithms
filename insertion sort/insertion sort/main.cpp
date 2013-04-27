abfb41368c121171872bf2ef8c927b2c4597daa5
lancebai <lance@gmail.com>
2013-04-27 02:43:23 +0100

    1st commit

---
+++ insertion sort/insertion sort/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  insertion sort
+//
+//  Created by Lance Bai on 13/4/27.
+//  Copyright (c) 2013年 Lance Bai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
